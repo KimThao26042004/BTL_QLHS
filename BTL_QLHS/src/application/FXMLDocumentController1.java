e2384f1d92993061ee0c76f96a1ece5980e14846
KimThao26042004 <130901332+KimThao26042004@users.noreply.github.com>
2024-05-13 18:14:44 +0700

    xong phan cung giao dien

---
+++ BTL_QLHS/src/application/FXMLDocumentController1.java
@@ -0,0 +1,49 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class FXMLDocumentController1 {
+
+    @FXML
+    private Button home_btn;
+
+    @FXML
+    private ImageView home_infor_btn;
+
+    @FXML
+    private ImageView home_score_btn;
+
+    @FXML
+    private Button infor_btn;
+
+    @FXML
+    private Label school;
+
+    @FXML
+    private Button score_btn;
+
+    @FXML
+    private AnchorPane studen_changePass_page;
+
+    @FXML
+    private AnchorPane student__home_page;
+
+    @FXML
+    private AnchorPane student_information_page;
+
+    @FXML
+    private AnchorPane student_left_scene;
+
+    @FXML
+    private AnchorPane student_right_scene;
+
+    @FXML
+    private AnchorPane student_top_scene;
+
+    @FXML
+    private AnchorPane student_transcript_page;
+}
